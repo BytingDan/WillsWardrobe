788933fb93db4a9027ba594b9c854299b49cc873
danjones123 <danielj.798@gmail.com>
2025-01-06 13:16:30 +0000

    Created boilerplate spring boot app

---
+++ src/main/java/com/willswardrobe/WillsWardrobe/WillsWardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.willswardrobe.WillsWardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillsWardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillsWardrobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willswardrobe/WillsWardrobe/WillsWardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willswardrobe.WillsWardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillsWardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

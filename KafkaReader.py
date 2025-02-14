ccba7479f2c1024c7bfc140e90c5ed1810d550b8
danjones123 <danielj.798@gmail.com>
2025-02-14 13:35:47 +0000

    Updated search to retrieve price

--- KafkaReader.py
+++ KafkaReader.py
@@ -28,9 +28,9 @@ def consume_messages():
                 print(f"Consumer error: {msg.error()}")
                 continue
 
-            print(type(msg))
-            print(type(msg.value()))
-            print(type(msg.key()))
+            # print(type(msg))
+            # print(type(msg.value()))
+            # print(type(msg.key()))
             decoded = msg.value().decode("utf-8")
             print(f"Received Message: {decoded}")
 
@@ -38,8 +38,9 @@ def consume_messages():
                 message_json = json.loads(decoded)
                 print("Parsed JSON: ", message_json)
 
-                url = message_json['itemName']
-                scrap_page(url)
+                url = message_json['itemURL']
+                image_url = message_json['imageURL']
+                scrap_page(url, image_url)
             except json.JSONDecodeError as e:
                 print(f"Error parsing JSON: {e}")
 
@@ -50,21 +51,35 @@ def consume_messages():
         consumer.close()
 
 
-def scrap_page(url):
+def scrap_page(url, image_url):
     try:
+        print("******")
         response = requests.get(url, headers={"User-Agent":"Mozilla/5.0"})
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
 
         title = soup.title.string if soup.title else "No title found"
+        print(f"Title: {title}")
         meta_desc = soup.find("meta", attrs={"name":"description"})
         description = meta_desc["content"] if meta_desc else "No Description Found"
-
-        print(f"Title: {title}")
         print(f"Description: {description}")
+
+        price = soup.find("span", class_="edbe20 ac3d9e d9ca8b").text.strip()
+        print("Price: ", price)
+
+        # colour = soup.find("span", class_="product-color").text.strip()
+        # print("Colour:", colour)
 
         images = [img["src"] for img in soup.find_all("img", src = True)]
         print(f"Found {len(images)} images.")
+        # print("Images:)
+
+        print("image URL: ", image_url)
+
+
+#TODO data to extrac
+# -> Title, Images, colours, sizes, price
+
 
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch URL: {e}")
